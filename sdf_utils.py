ee2420d03f442d2dc64c7fb41ed440f69b1bbdb7
shenjiaming <shenjiaming@rd.netease.com>
2022-08-02 22:54:52 +1000

    vis sdf

--- sdf_utils.py
+++ sdf_utils.py
@@ -15,8 +15,8 @@ def plot_sdf_predicted(z_vals, predicted_sdf):
         predicted_sdf: (1, n_samples)
     """
     plt.figure(figsize=(6, 6))
-    z_vals = z_vals.detach().numpy().reshape(-1)
-    predicted_sdf = predicted_sdf.detach().numpy().reshape(-1)
+    z_vals = z_vals.detach().cpu().numpy().reshape(-1)
+    predicted_sdf = predicted_sdf.detach().cpu().numpy().reshape(-1)
     plt.plot(z_vals, predicted_sdf, label='predicted_sdf')
     plt.plot(z_vals, np.zeros_like(z_vals), '--')
     plt.legend()
